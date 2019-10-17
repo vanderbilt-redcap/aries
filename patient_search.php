4bd01930609b994f4fbb463c2feaac50d1a6a60e
Carl Reed <carlreedw@gmail.com>
2019-10-17 15:36:29 -0500

    implementing search interface

--- patient_search.php
+++ patient_search.php
@@ -5,5 +5,25 @@ require_once APP_PATH_DOCROOT . 'ProjectGeneral' . DIRECTORY_SEPARATOR. 'header.
 <link rel="stylesheet" href="<?=$module->getUrl('search.css')?>"/>
 <script type="text/javascript" src="<?=$module->getUrl('search.js')?>"></script>
 
+<div id='header' class='row'>
+	<div class='logo'>
+		<span id='xdro-title'>xdro</span>
+		<img id='tdh-logo' src="<?=$module->getUrl('tdh-logo.png')?>"></img>
+	</div>
+	<div id='registry-title'>
+		<h1>Extensively Drug Resistant Organism Registry</h1>
+	</div>
+</div>
+
+<div id="search" class='row col-9'>
+	<div id='search-info' class='col-4'>
+		<h5><b>Search Query</b></h5>
+		<p>Begin typing to search the registry data,<br> then click an item in the list to navigate to that record for further investigation.</p>
+	</div>
+	<div id="search-input" class='col-8'>
+		
+	</div>
+</div>
+
 <?php
 require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
