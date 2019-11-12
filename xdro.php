9bba264393000bb3dbdf3ca514430743376fa653
Carl Reed <carl.w.reed@vumc.org>
2019-11-12 11:40:34 -0600

    semicolon addition

--- xdro.php
+++ xdro.php
@@ -8,6 +8,6 @@ class XDRO extends \ExternalModules\AbstractExternalModule {
 	
 	// given a user supplied string, search for records in our patient registry that might match
 	function patient_search($search_string) {
-		$name1, $name2, $date = "", "", ""
+		// $name1, $name2, $date = "", "", "";
 	}
 }
